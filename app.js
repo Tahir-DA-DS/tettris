3380d40f88bb4cc357c1a21f73fea8c9694ca90c
Tahir-DA-Ds <tahir.adeleye@gmail.com>
2024-12-12 15:46:09 +0100

    directions

--- app.js
+++ app.js
@@ -66,7 +66,21 @@ const zShape = [
 
   //move shape down every sec
 
-  let timerId = setInterval(moveDown, 1000);
+  timerId = setInterval(moveDown, 1000);
+
+  function control(e){
+    if(e.keyCode === 37){
+        moveLeft()
+    } else if(e.keyCode === 38){
+        //rotate()
+    }else if(e.keyCode === 39){
+        moveRight()
+    }else if(e.keyCode === 40){
+        moveDown()
+    }
+  }
+
+  document.addEventListener('keyup', control)
 
   function moveDown (){
     undraw()
@@ -101,6 +115,15 @@ function moveLeft(){
     draw()
 }
 
+function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+    if(!isAtRightEdge) currentPosition +=1
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -=1
+    }
+    draw()
+  }
 
 
 
