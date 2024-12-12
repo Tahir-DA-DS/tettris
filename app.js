ccc226162836f030b4d91fae5f4f00d5838a062a
Tahir-DA-Ds <tahir.adeleye@gmail.com>
2024-12-12 13:24:01 +0100

    shapes

--- app.js
+++ app.js
@@ -4,6 +4,43 @@ let squares = Array.from(document.querySelectorAll('.grid div'))
 const scoreDisplay = document.querySelector('#score')
 const startBtn = document.querySelector('#start-button')
 const width = 10
-console.log(squares);
+
+
+//shapes
+const lShape = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+]
+
+const zShape = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tShape = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oShape = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iShape = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
 
 })
+
